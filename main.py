c36795d0b939bcbaf481dc3a48ecf2ee66123233
ac-alpha <ashutoshchaubey2999@gmail.com>
2018-11-27 02:11:17 +0530

    Added data loading code

---
+++ main.py
@@ -0,0 +1,27 @@
+from os import listdir
+from os.path import isfile, join
+
+'''First step : Loading the dataset'''
+
+dataSetPath = "data/names/"
+onlyFileNames = []
+for f in listdir(dataSetPath):
+	if isfile(join(dataSetPath, f)):
+		if f[:2]!="._":
+			onlyFileNames.append(f)
+# print(onlyFileNames)			#for debugging
+
+dataSetComplete={}
+completeFileName=""
+
+for file in onlyFileNames:
+	completeFileName = dataSetPath+file
+	ofile = open(completeFileName,"r")
+	currentFileData = ofile.readlines()
+	currentFileData = [x.strip() for x in currentFileData]
+	ofile.close()
+	langName = file[:-4]
+	dataSetComplete[langName]=currentFileData
+# print(dataSetComplete["Czech"])		#for debugging
+
+
